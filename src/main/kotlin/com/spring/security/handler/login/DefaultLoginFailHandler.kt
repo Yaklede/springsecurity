4a083ed7c7f606c7be5889ce988b2fb5254d8401
Yaklede <chh78151@naver.com>
2023-04-12 08:46:47 +0900

    feature: login Handler 작업

---
+++ src/main/kotlin/com/spring/security/handler/login/DefaultLoginFailHandler.kt
@@ -0,0 +1,21 @@
+package com.spring.security.handler.login
+
+import com.spring.security.infoLog
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.authentication.AuthenticationFailureHandler
+
+class DefaultLoginFailHandler : AuthenticationFailureHandler {
+
+    override fun onAuthenticationFailure(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        exception: AuthenticationException
+    ) {
+        infoLog {
+            "authentication Fail"
+        }
+        response.sendRedirect("/login")
+    }
+}
---
+++ src/main/kotlin/com/spring/security/handler/login/DefaultLoginSuccessHandler.kt
@@ -0,0 +1,20 @@
+package com.spring.security.handler.login
+
+import com.spring.security.infoLog
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+
+class DefaultLoginSuccessHandler : AuthenticationSuccessHandler {
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        infoLog {
+            "authentication ${authentication.name}"
+        }
+        response.sendRedirect("/")
+    }
+}
