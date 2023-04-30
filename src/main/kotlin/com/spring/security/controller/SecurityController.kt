38eaaf41a6d08bad6f1b496ea96fe413aa2165c6
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-01 00:50:57 +0900

    test: test를 위한 endpoint 추가

--- src/main/kotlin/com/spring/security/controller/SecurityController.kt
+++ src/main/kotlin/com/spring/security/controller/SecurityController.kt
@@ -1,14 +1,29 @@
 package com.spring.security.controller
 
+import com.spring.security.domain.member.entity.MemberRole
+import com.spring.security.domain.member.entity.dto.MemberDto
+import com.spring.security.domain.member.service.MemberService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class SecurityController {
-
+class SecurityController(
+    private val memberService: MemberService
+) {
     @GetMapping("/")
     fun index(): String {
         return "home"
+    }
+
+    @GetMapping("/join")
+    fun join(): MemberDto {
+        val member = MemberDto.mock().apply {
+            this.username = "user"
+            this.loginId = "loginId"
+            this.password = "pass"
+            this.roles = mutableListOf(MemberRole(role = "ADMIN"))
+        }
+        return memberService.join(member)
     }
 
     @GetMapping("/loginPage")
