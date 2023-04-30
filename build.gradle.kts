33452c89708b16996f5e7afa5b71702995feb856
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-01 00:48:25 +0900

    fix: 의존성, git ignore setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,10 @@ repositories {
 }
 
 dependencies {
-	implementation("com.querydsl:querydsl-jpa:5.0.0")
-	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+	implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+	implementation("com.querydsl:querydsl-core:5.0.0")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+	kapt("com.querydsl:querydsl-kotlin-codegen:5.0.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -28,7 +30,7 @@ dependencies {
 	implementation("org.graalvm.nativeimage:svm:21.3.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
-	runtimeOnly("com.h2database:h2")
+	implementation("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 }
