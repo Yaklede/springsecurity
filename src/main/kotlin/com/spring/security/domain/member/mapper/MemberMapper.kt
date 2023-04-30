76892b74561ea4786a0ecb2f4ad1697c79c5a9d1
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-01 00:48:07 +0900

    feature: mapper추가

--- src/main/kotlin/com/spring/security/domain/member/mapper/MemberMapper.kt
+++ src/main/kotlin/com/spring/security/domain/member/mapper/MemberMapper.kt
@@ -1,4 +1,30 @@
 package com.spring.security.domain.member.mapper
 
+import com.spring.security.domain.member.entity.Member
+import com.spring.security.domain.member.entity.dto.MemberDto
+
 object MemberMapper {
+    fun toEntity(dto: MemberDto): Member {
+        return Member(
+            id = dto.id,
+            username = dto.username,
+            loginId = dto.loginId,
+            password = dto.password,
+            email = dto.email,
+            age = dto.age,
+            roles = dto.roles
+        )
+    }
+
+    fun toDto(entity: Member): MemberDto {
+        return MemberDto(
+            id = entity.id,
+            username = entity.username,
+            loginId = entity.loginId,
+            password = entity.password,
+            email = entity.email,
+            age = entity.age,
+            roles = entity.roles
+        )
+    }
 }
--- src/test/kotlin/com/spring/security/domain/member/mapper/MemberMapperTest.kt
+++ src/test/kotlin/com/spring/security/domain/member/mapper/MemberMapperTest.kt
@@ -1,16 +1,45 @@
 package com.spring.security.domain.member.mapper
 
+import com.spring.security.domain.member.entity.Member
+import com.spring.security.domain.member.entity.dto.MemberDto
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-
-import org.junit.jupiter.api.Assertions.*
 
 class MemberMapperTest {
 
     @Test
     fun toEntity() {
+        //given
+        val memberDto = MemberDto.mock()
+
+        //when
+        val member = MemberMapper.toEntity(memberDto)
+
+        //then
+        assertThat(member).isInstanceOf(Member::class.java)
+        assertThat(member.id).isEqualTo(memberDto.id)
+        assertThat(member.username).isEqualTo(memberDto.username)
+        assertThat(member.age).isEqualTo(memberDto.age)
+        assertThat(member.loginId).isEqualTo(memberDto.loginId)
+        assertThat(member.password).isEqualTo(memberDto.password)
+        assertThat(member.email).isEqualTo(memberDto.email)
     }
 
     @Test
     fun toDto() {
+        //given
+        val member = Member.mock()
+
+        //when
+        val memberDto = MemberMapper.toDto(member)
+
+        //then
+        assertThat(memberDto).isInstanceOf(MemberDto::class.java)
+        assertThat(member.id).isEqualTo(memberDto.id)
+        assertThat(member.username).isEqualTo(memberDto.username)
+        assertThat(member.age).isEqualTo(memberDto.age)
+        assertThat(member.loginId).isEqualTo(memberDto.loginId)
+        assertThat(member.password).isEqualTo(memberDto.password)
+        assertThat(member.email).isEqualTo(memberDto.email)
     }
 }
