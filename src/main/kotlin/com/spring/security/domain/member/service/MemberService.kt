a6e825135911ab6710c7a729334654ffe071fc3d
Yaklede <68309983+Yaklede@users.noreply.github.com>
2023-05-01 00:48:58 +0900

    feature: memberService추가

--- src/main/kotlin/com/spring/security/domain/member/service/MemberService.kt
+++ src/main/kotlin/com/spring/security/domain/member/service/MemberService.kt
@@ -1,4 +1,31 @@
 package com.spring.security.domain.member.service
 
-class MemberService {
+import com.spring.security.domain.member.entity.dto.MemberDto
+import com.spring.security.domain.member.mapper.MemberMapper
+import com.spring.security.domain.member.repository.MemberRepository
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class MemberService(
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: PasswordEncoder,
+) {
+    @Transactional
+    fun join(memberDto: MemberDto): MemberDto {
+        memberDto.password?.let {
+            memberDto.password = passwordEncoder.encode(it)
+        }
+        val member = MemberMapper.toEntity(memberDto)
+        memberRepository.save(member).let {
+            return MemberMapper.toDto(it)
+        }
+    }
+
+    fun findMemberById(memberId: Long): MemberDto {
+        memberRepository.findById(memberId).orElseThrow { throw IllegalArgumentException("회원이 존재하지 않습니다.") }.let {
+            return MemberMapper.toDto(it)
+        }
+    }
 }
